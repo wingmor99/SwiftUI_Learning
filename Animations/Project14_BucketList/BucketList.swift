33bf283dd762bdafcb9f9c9a19b85a2f6427394c
linlin <wingmor51@gmail.com>
2020-02-08 20:22:58 -0500

    Add the biometrics to unlock the data

--- Animations/Project14_BucketList/BucketList.swift
+++ Animations/Project14_BucketList/BucketList.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Jinchao Hou. All rights reserved.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -15,38 +16,49 @@ struct BucketList: View {
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEdit = false
+    @State private var isUnlocked = false
     
     var body: some View {
         ZStack {
-            ProjectMapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack{
-                Spacer()
-                HStack{
+            if isUnlocked {
+                ProjectMapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                    .edgesIgnoringSafeArea(.all)
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                
+                VStack{
                     Spacer()
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.title = "Example Location"
-                        newLocation.coordinate = self.centerCoordinate
-                        self.locations.append(newLocation)
-                        
-                        self.selectedPlace = newLocation
-                        self.showingEdit = true
-                    }) {
-                        Image(systemName: "plus")
-                    }  
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            let newLocation = CodableMKPointAnnotation()
+                            newLocation.title = "Example Location"
+                            newLocation.coordinate = self.centerCoordinate
+                            self.locations.append(newLocation)
+                            
+                            self.selectedPlace = newLocation
+                            self.showingEdit = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                    }
                 }
+            } else {
+                Button("Unlock places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -87,6 +99,26 @@ struct BucketList: View {
             try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
         } catch {
             print("Unable to save data")
+        }
+    }
+    
+    // add the faceID or touchID
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authentivate yourself to unlock your places"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {success, anthenticationError in DispatchQueue.main.async {
+                if success {
+                    self.isUnlocked = true
+                } else {
+                    //error
+                }
+                }}
+        } else {
+            // no bio now
         }
     }
 }
