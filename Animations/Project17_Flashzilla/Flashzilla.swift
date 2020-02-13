b5e52c1be0fb24b799c5b9eccca7bafbf82fe259
linlin <wingmor51@gmail.com>
2020-02-13 09:11:18 -0500

    add Multiple cards in contenView

--- Animations/Project17_Flashzilla/Flashzilla.swift
+++ Animations/Project17_Flashzilla/Flashzilla.swift
@@ -6,11 +6,37 @@
 //  Copyright © 2020 Jinchao Hou. All rights reserved.
 //
 
+// https://www.hackingwithswift.com/books/ios-swiftui/building-a-stack-of-cards
+
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
+    }
+}
+
 struct Flashzilla: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    
+                    
+                    ForEach(0..<cards.count, id: \.self) {index in
+                        
+                        SingleCaedView(card: self.cards[index])
+                            .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
